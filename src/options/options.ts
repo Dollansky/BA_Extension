155632ddb2e466caf46c53d507a8aa7c928226bf
Dollansky <49188175+Dollansky@users.noreply.github.com>
2021-04-15 11:51:53 +0200

    Table finished, grid needed

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,7 +1,4 @@
-import {MDCTextField} from '@material/textfield';
-import {MDCRipple} from '@material/ripple'
-import {MDCList} from "@material/list/component";
-import onButtonClicked = chrome.notifications.onButtonClicked;
+
 
 // TODO Layout Überschrift  remove functionality
 // TODO blacklist -> Object Blacklist: domain , intervention, default antworten
@@ -9,40 +6,74 @@ window.console.log("hi");
 let blacklist: Array<string>;
 chrome.storage.local.get(['blacklist'], (result) => {
 
-    let listContainer = document.createElement('div');
-    listContainer.setAttribute('class', 'collection');
+    let listContainer = document.getElementById('blacklist')
     let listElement = document.createElement('ul');
-    document.getElementsByTagName('body')[0].appendChild(listContainer);
-    document.getElementById('blacklist').appendChild(listContainer);
     listContainer.appendChild(listElement);
     blacklist = result.blacklist;
 
 
     blacklist.forEach(function(domain) {
-       let listItem = document.createElement('li');
-       listItem.setAttribute('class','collection-item');
-       let img = document.createElement('img');
-       img.src = "chrome://favicon/https:/" + domain;
-       listItem.appendChild(img);
-       listItem.innerHTML = domain;
-       let container = document.createElement('div');
-       container.className = "secondary-content";
-       let removeButton = document.createElement('button');
-       removeButton.id = domain;
-       removeButton.className = "waves-effect waves-light btn";
-       let removeIcon= document.createElement('i');
-       removeIcon.setAttribute('class','material-icons');
-       removeIcon.innerText = "remove_circle";
-       removeButton.appendChild(removeIcon);
-       removeButton.addEventListener('click', function() {
-           removeDomain(domain);
-       }, false);
-       container.appendChild(removeButton);
-       listItem.appendChild(container);
-       listElement.appendChild(listItem);
+        let tableRow = document.createElement('tr');
+
+        // Icon create
+        let icon = document.createElement('td');
+        let img = document.createElement('img');
+        img.src = "chrome://favicon/https:/" + domain;
+        icon.appendChild(img);
+
+        // Domain setting
+        let domainTD = document.createElement('td');
+        let domainText = document.createElement('p');
+        domainText.setAttribute('font-size', '23')
+        domainText.innerText = domain;
+        domainTD.appendChild(domainText);
+
+
+        // Button creation
+        let buttonTD = document.createElement('td');
+        buttonTD.className = 'right-align';
+        let removeButton = document.createElement('button');
+        removeButton.id = domain;
+        removeButton.className = "waves-effect waves-light btn";
+        let removeIcon= document.createElement('i');
+        removeIcon.setAttribute('class','material-icons');
+        removeIcon.innerText = "remove_circle";
+        removeButton.appendChild(removeIcon);
+        removeButton.addEventListener('click', function() {
+            removeDomain(domain);
+        }, false);
+        buttonTD.appendChild(removeButton);
+        tableRow.append(icon,domainTD,buttonTD);
+        let table = document.getElementById('blacklist');
+        table.appendChild(tableRow);
+
+       // let listItem = document.createElement('li');
+       // listItem.setAttribute('class','collection-item');
+       // let img = document.createElement('img');
+       // img.src = "chrome://favicon/https:/" + domain;
+       //
+       // listItem.innerHTML = domain;
+       // listItem.appendChild(img);
+       // let container = document.createElement('div');
+       // container.className = "secondary-content";
+       // let removeButton = document.createElement('button');
+       // removeButton.id = domain;
+       // removeButton.className = "waves-effect waves-light btn";
+       // let removeIcon= document.createElement('i');
+       // removeIcon.setAttribute('class','material-icons');
+       // removeIcon.innerText = "remove_circle";
+       // removeButton.appendChild(removeIcon);
+       // removeButton.addEventListener('click', function() {
+       //     removeDomain(domain);
+       // }, false);
+       // container.appendChild(removeButton);
+       // listItem.appendChild(container);
+       // listElement.appendChild(listItem);
     })
 })
 
+
+// TODO add functionality
 function removeDomain(domain: string) {
     window.console.log(domain);
 
