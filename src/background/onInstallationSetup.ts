955211e8e8e70c24898758db595e336fc6e2b056
Dollansky <49188175+Dollansky@users.noreply.github.com>
2022-03-09 13:14:57 +0100

    removed duplicate code

--- src/background/onInstallationSetup.ts
+++ src/background/onInstallationSetup.ts
@@ -10,37 +10,6 @@ import {
 
 
 
-chrome.storage.local.get(['blacklist', 'baselineFinished','previousGoals','lastDomain','activeWebsites','mode','dateWhenModeEnds','einwilligung'], (result) => {
-    if (result.blacklist == undefined) {
-        const blacklist: Array<string> = ["www.instagram.com", "www.facebook.com", "www.youtube.com", "www.netflix.com", "www.twitch.tv"];
-        chrome.storage.local.set({blacklist: blacklist});
-        chrome.bookmarks.create({ parentId: '1', title: 'Blacklist Extension', url: browserUrl + 'options/options.html' });
-        chrome.browserAction.setIcon({path: 'img/work.png'});
-    }
-    if (result.baselineFinished === undefined || result.baselineFinished == null) {
-        var today = new Date();
-        chrome.storage.local.set({baselineFinished: [today.getUTCDate() + 7, today.getUTCMonth() , today.getUTCFullYear()]});
-    }
-    if(result.previousGoals == undefined){
-        chrome.storage.local.set({previousGoals: []});
-    }
-    if(result.lastDomain == undefined){
-        chrome.storage.local.set({lastDomain: {domain: "Installation Time"}})
-    }
-    if(result.activeWebsites == undefined){
-        chrome.storage.local.set({activeWebsites: []});
-    }
-    if(result.mode == undefined){
-        sendMessageToEveryTab("Open Mode Select");
-    }
-    if(result.dateWhenModeEnds == undefined){
-        chrome.storage.local.set( {dateWhenModeEnds: Date.now() + 100000})
-    }
-    if(result.einwilligung == undefined){
-        chrome.storage.local.set({einwilligung: false})
-    }
-})
-
 export function onInstalledDo() {
     chrome.storage.local.get(['blacklist', 'baselineFinished', 'previousGoals', 'lastDomain', 'activeWebsites', 'mode', 'dateWhenModeEnds', 'participantId','einwilligung'], (result) => {
         if (result.blacklist == undefined) {
